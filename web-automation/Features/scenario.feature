2d1a76e6c7ecb84798fdb1695c1842bd6f307f99
fred4impact <runtesting4me@gmail.com>
2018-03-09 00:23:06 +0000

    Sample project files

---
+++ web-automation/Features/scenario.feature
@@ -0,0 +1,7 @@
+Feature: Login Feature
+
+
+Scenario: verify user can login using valid username and password
+  Given A user is on facebook login page
+  Then User is able to login Succesfully
+   
---
+++ web-automation/src/main/java/Runner/CucumberRunner.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package Runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+/**
+ * @author apple
+ *here is my runner that runs my test script
+ */
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Features",
+		glue= {""}
+		)
+
+public class CucumberRunner {
+
+
+}
